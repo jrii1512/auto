7c8703bdc7fb521a7ad25d534d3c947272922efb
Jouni <jriimala@gmail.com>
2021-12-17 11:29:03 +0200

    deno deployment

---
+++ app-launch.js
@@ -0,0 +1,3 @@
+import {app} from "./app.js";
+
+await app.listen({port:5000});
---
+++ app.js
@@ -0,0 +1,8 @@
+import { Application } from "https://deno.land/x/oak@v10.0.0/mod.ts";
+
+const app = new Application();
+app.use((context, next) => {
+    context.response = "Hellow world!";
+});
+
+export {app};
