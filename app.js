c4ee5cfd4b8643f7b777ca822f175f2fcf4ffb0e
jrii1512 <62070396+jrii1512@users.noreply.github.com>
2021-12-17 11:34:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 
 const app = new Application();
 app.use((context, next) => {
-    context.response = "Hellow world!";
+    context.response.body = "Hello world!";
 });
 
 export {app};
